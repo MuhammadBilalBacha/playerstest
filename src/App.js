a9939cdbe9c32b58288a0d7907294917f9390628
Bilal <bilal.se.iiui@gmail.com>
2023-04-10 02:46:16 +0500

    Test Helmet

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import "./App.css";
 import PlayerList from "./Components/PlayerList/PlayerList";
 import { Route, Routes } from "react-router-dom";
 import PlayerDetails from "./Components/PlayerDetails/PlayerDetails";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div className="App">
       <div className="container py-5">
+        <Helmet>
+          <meta
+            property="og:image"
+            itemprop="image"
+            content="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg?v=1"
+            id="og-image"
+          />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="600" />
+        </Helmet>
         <h3>Players Data</h3>
       </div>
       <Routes>
