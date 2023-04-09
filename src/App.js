7c91363d265cb3adfa1bbb7496eb842647414d6b
Bilal <bilal.se.iiui@gmail.com>
2023-04-10 04:15:10 +0500

    Test Helmet and Remove head

--- src/App.js
+++ src/App.js
@@ -3,20 +3,35 @@ import "./App.css";
 import PlayerList from "./Components/PlayerList/PlayerList";
 import { Route, Routes } from "react-router-dom";
 import PlayerDetails from "./Components/PlayerDetails/PlayerDetails";
-// import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta charset="utf-8" />
+        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta
+          name="description"
+          content="Web site created using create-react-app"
+        />
+        <meta
+          property="og:image"
+          itemprop="image"
+          content="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg?v=1"
+          id="og-image"
+        />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="600" />
+        <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
+
+        <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+
+        <title>React App</title>
+      </Helmet>
       <div className="container py-5">
-        <head>
-          <meta
-            property="og:image"
-            itemprop="image"
-            content="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg?v=1"
-            id="og-image"
-          />
-        </head>
         <h3>Players Data</h3>
       </div>
       <Routes>
