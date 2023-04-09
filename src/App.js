756232495e6a5076c89c5ac955edff97994f0fc4
Bilal <bilal.se.iiui@gmail.com>
2023-04-10 03:05:49 +0500

    Change Helmet to Head

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import "./App.css";
 import PlayerList from "./Components/PlayerList/PlayerList";
 import { Route, Routes } from "react-router-dom";
 import PlayerDetails from "./Components/PlayerDetails/PlayerDetails";
-import { Helmet } from "react-helmet";
+// import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div className="App">
       <div className="container py-5">
-        <Helmet>
+        <head>
           <meta
             property="og:image"
             itemprop="image"
             content="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg?v=1"
             id="og-image"
           />
-        </Helmet>
+        </head>
         <h3>Players Data</h3>
       </div>
       <Routes>
